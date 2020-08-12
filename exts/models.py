da5938570d863a12f4fd339837f1e2c63c686e6a
sean.yang <sean.yang@ly.com>
2020-08-12 17:43:23 +0800

    test

--- exts/models.py
+++ exts/models.py
@@ -45,27 +45,10 @@ api = Api(app=app)
 #解析器
 parser =reqparse.RequestParser()
 parser.add_argument('gid', type=int, help='请输入商品编号', required=True, location=['form', 'args'])
-parser.add_argument('gname', type=str, help='请输入商品名称', required=True, location=['form', 'args'])
+parser.add_argument('name', type=str, help='请输入商品名称', required=True, location=['form', 'args'])
 parser.add_argument('price', type=int, help='请输入商品价格', required=True, location=['form', 'args'])
 parser.add_argument('number', type=int, help='请输入商品数量', location=['form'])
 parser.add_argument('status', type=str, choices=['聚划算商品', '非聚划算商品'], help='必须正确填写状态', required=True, location=['form', 'args'])
-
-goods_fields = {
-    'gid': fields.Integer,
-    'gname': fields.String,
-    'price': fields.Integer,
-    'number': fields.Integer,
-    'status': fields.String
-}
-
-goods_obj_fields = {
-    'msg': fields.String,
-    'goods': fields.Nested(goods_fields)
-}
-
-goods_list_fields = {
-    'goodsList': fields.List(fields.Nested(goods_fields))
-}
 
 #定义Resource子类
 class GoodsResource(Resource):
@@ -80,24 +63,19 @@ class GoodsResource(Resource):
 
 class GoodsListResource(Resource):
 
-    # def get(self):
-    #     return marshal(goods_list, goods_fields)
-
-    @marshal_with(goods_list_fields)
     def get(self):
         goods_list = goods.query.all()
         return {'goodsList': goods_list}
 
     #商品的添加
-    @marshal_with(goods_obj_fields)
     def post(self):
         args = parser.parse_args()
         gid = args.get('gid')
-        gname = args.get('gname')
+        name = args.get('name')
         price = args.get('price')
         number = args.get('number')
         status = args.get('status')
-        tmpgood = goods(gid, gname, price, status)
+        tmpgood = goods(gid, name, price, status)
         if number and number > 0:
             tmpgood.number = number
         db.session.add_all([tmpgood])
